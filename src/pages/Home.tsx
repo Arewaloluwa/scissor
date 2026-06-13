f747d67e859a8bb1f20810003f86ea38e12457c2
Arewaloluwa <arewaakinbayo@gmail.com>
2026-06-13 23:46:39 +0200

    updated home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,22 @@
-export default function App() {
+import { UserButton } from "@clerk/clerk-react";
+import ShortenForm from "../components/ShortenForm";
+import QRCodeDisplay from "../components/QRCodeDisplay";
+
+export default function Home() {
   return (
-    <h1>APP IS RENDERING</h1>
+    <div className="max-w-4xl mx-auto p-8">
+      <div className="flex justify-between mb-8">
+       <div>
+      <QRCodeDisplay value="" />
+    </div>
+        <h1 className="text-4xl font-bold">
+          Scissor
+        </h1>
+
+        <UserButton />
+      </div>
+
+      <ShortenForm />
+    </div>
   );
 }
